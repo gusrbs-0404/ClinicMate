cc8030e5e7142d532f27e81b8e4e4dc4a581381d
gusrbs-0404 <gusrbs7534@gmail.com>
2025-09-25 17:08:50 +0900

    Create Project

---
+++ src/main/java/com/example/ClinicMate/ClinicMateApplication.java
@@ -0,0 +1,13 @@
+package com.example.ClinicMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ClinicMate/ClinicMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ClinicMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
